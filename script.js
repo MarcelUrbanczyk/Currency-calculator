fc366687ca9485c111307baed46906350d59ae14
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-05-23 17:55:37 +0200

    js refactor

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-let resultOutput = document.getElementById("js-result");
-let submitButton = document.getElementById("js-submit");
-let selectedOption = document.querySelector(".form__select");
-let customCurrency = document.getElementById("js-customCurrency");
+{
+  const selectedOption = document.querySelector(".form__select");
+  const customCurrency = document.getElementById("js-customCurrency");
 
-selectedOption.addEventListener("change", () => {
-  if (selectedOption.value === "Custom currency") {
-    customCurrency.classList.remove("form__input--display");
-  } else {
-    customCurrency.classList.add("form__input--display");
-  }
-});
 
-submitButton.addEventListener("click", () => {
-  let amount = document.getElementById("js-amount").value;
-  let selectedOption = document.querySelector(".form__select").value;
-  switch (selectedOption) {
-    case "USD":
-      result = amount * 4.24;
-      break;
-
-    case "EUR":
-      result = amount * 4.63;
-      break;
-
-    case "GBP":
-      result = amount * 5.24;
-      break;
-
-    case "Custom currency":
-      result = amount * customCurrency.value;
-      break;
+  const inputPopup = () =>{
+    if (selectedOption.value === "Custom currency") {
+      customCurrency.classList.remove("form__input--display");
+    } else {
+      customCurrency.classList.add("form__input--display");
+    }
   }
 
+
+
+const formSubmit = () =>{
+  const amount = document.getElementById("js-amount").value;
+  let result= calculateResult(amount)
+  resultPopup(result);
+
+}
+
+const resultPopup = (result) =>{
+  const resultOutput = document.getElementById("js-result");
   if (result > 0) {
     resultOutput.innerHTML =
-      "Amount is: " + result.toFixed(2) + "PLN";
+      "Amount is: " + result.toFixed(2) + " " + selectedOption.value;
   } else {
     resultOutput.innerHTML = "Invalid data";
   }
-});
+
+}
+
+ const calculateResult = (amount) =>{
+  const rateUSD=4.25
+  const rateEUR=4.63
+  const rateGBP=5.24
+  switch (selectedOption.value) {
+    case "USD":
+      return amount / rateUSD;
+
+    case "EUR":
+      return amount / rateEUR;
+
+    case "GBP":
+      return amount / rateGBP;
+
+    case "Custom currency":
+      return amount * customCurrency.value;
+  }
+  
+ }
+
+ const init=()=>{
+  const submitButton = document.getElementById("js-submit");
+  selectedOption.addEventListener("change",inputPopup)
+  submitButton.addEventListener("click",formSubmit)
+
+ }
+init();
+}
